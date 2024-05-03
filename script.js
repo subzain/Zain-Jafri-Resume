bb11baadb9a5bd13fe556666b4ebae3e26cc78e0
subzain <syedmjafri.zain@gmail.com>
2024-05-03 16:36:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const courseCards = document.querySelectorAll('.course-card');
-
-courseCards.forEach(card => {
-  card.addEventListener('click', () => {
-    const courseDetails = card.querySelector('.course-details');
-    courseDetails.classList.toggle('active'); // Toggle active class for popup display
-  });
-});
 
